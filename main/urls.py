f42faadc7a94ad407ae0a5bab871ab3f7558fbf7
Sofia <smmilyukova@gmail.com>
2022-07-10 15:22:37 +0300

    Добавляет в проект Angular

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from articles import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('articles/', views.articles),
-]
